320c13891ee2c41a6271b24674c83aed85c7ba78
Albert I <kras@raphielgang.org>
2019-08-02 20:54:05 +0800

    ak3: Rip ramdisk
    Signed-off-by: Albert I <kras@raphielgang.org>

--- anykernel.sh
+++ anykernel.sh
@@ -25,12 +25,6 @@ ramdisk_compression=auto;
 . tools/ak3-core.sh;
 
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
 ## AnyKernel install
 dump_boot;
 
